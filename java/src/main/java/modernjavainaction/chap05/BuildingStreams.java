6101e473939cd31f6f549baee06bc98dd3940666
Bill Lyons <blyons3@yahoo.com>
2023-06-20 13:36:06 -0600

    Fixed a path reference in BuildingStreams.java. It was throwing an exception. Also, added a try with resources.  Path and lines are autocloseable.

--- java/src/main/java/modernjavainaction/chap05/BuildingStreams.java
+++ java/src/main/java/modernjavainaction/chap05/BuildingStreams.java
@@ -3,7 +3,6 @@ package modernjavainaction.chap05;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.function.IntSupplier;
